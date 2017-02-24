cc4cb96b9aba04d17912dd887fbdd9f9ef4b987c
tre <tre@TRE->
2017-02-24 09:45:59 +0600

    Добавьте файлы проекта.

---
+++ web_it/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="web_it.Global" Language="C#" %>
---
+++ web_it/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(web_it.Startup))]
+namespace web_it
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
